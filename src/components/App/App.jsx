06ccde94befd9ff17bf7b78752dfbef09c69d3de
kulikp1 <pashakylik2@gmail.com>
2025-04-30 13:28:32 +0300

    update routes

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "../HomePage/HomePage";
 import LoginPage from "../LoginPage/LoginPage";
 import RegisterPage from "../RegisterPage/RegisterPage";
+import TaskPage from "../TaskPage/TaskPage";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
+        <Route path="/home" element={<TaskPage />} />
       </Routes>
     </Router>
   );
