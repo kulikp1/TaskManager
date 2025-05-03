a8c5ad756e538e548d1406dec7db423357c6667c
kulikp1 <pashakylik2@gmail.com>
2025-05-03 21:16:51 +0300

    update connection to bd settings

--- server/server.js
+++ server/server.js
@@ -20,8 +20,7 @@ app.use(cors({
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+  
 })
   .then(() => console.log('MongoDB підключено'))
   .catch(err => console.error('Помилка MongoDB:', err));
