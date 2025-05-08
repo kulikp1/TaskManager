b6d82c7e5b7c339ccdbfabff5c08651ce6052c5d
kulikp1 <pashakylik2@gmail.com>
2025-05-09 02:06:02 +0300

    update style for loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -3,8 +3,19 @@ import Style from "./Loader.module.css";
 
 const Loader = () => {
   return (
-    <div className={Style.loader}>
-      <div className={Style.spinner}></div>
+    <div className={Style.loaderWrapper}>
+      <div className={Style.column}>
+        <div className={Style.columnTitle}>
+          Loading<span className={Style.dot}>.</span>
+          <span className={Style.dot}>.</span>
+          <span className={Style.dot}>.</span>
+        </div>
+        <div className={Style.taskCard}>Task 1</div>
+        <div className={Style.taskCard}>Task 2</div>
+        <div className={Style.taskCard}>Task 3</div>
+        <div className={Style.taskCard}>Task 4</div>
+        <div className={Style.taskCard}>Task 5</div>
+      </div>
     </div>
   );
 };
